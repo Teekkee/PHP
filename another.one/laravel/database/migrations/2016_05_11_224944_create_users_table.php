33fd57b35db67679229169dcda555ff03620d313
Teekkee <lappe1418@gmail.com>
2016-06-12 12:21:11 +0300

    Laravel

---
+++ another.one/laravel/database/migrations/2016_05_11_224944_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->string('FName');
+	    $table->string('LName');
+	    $table->string('Login')-> unique();
+	    $table->string('Pass', 13);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user');
+    }
+}
